ebabfbe2aa913fc06a21c2b9af7e75aab83c01e1
Sharat Chandra <65823755+sharatmanikonda@users.noreply.github.com>
2025-07-08 10:14:56 +0530

    Update KNN.py

--- KNN.py
+++ KNN.py
@@ -8,7 +8,7 @@ from sklearn.metrics import accuracy_score, confusion_matrix, classification_rep
 import pickle
 
 # Load the dataset
-file_path = r"D:\Instilit\Trainings\material_PPTs\DS\13.KNN\Chronic_Kidney_Disease_data.csv"
+file_path = r"Chronic_Kidney_Disease_data.csv"
 data = pd.read_csv(file_path)
 
 # Drop non-predictive columns
